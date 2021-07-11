c80610524490f0801620cc06d4b531958929149d
oximenvn <oximenvn@gmail.com>
2021-07-11 10:01:58 +0700

    Update test.go

--- test/test.go
+++ test/test.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"reflect"
-	"context"
 	"time"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -15,39 +16,51 @@ type DataBase struct {
 	Name string
 }
 
+const shortDuration = 1 * time.Second
+
 func (db DataBase) make() {
 	fmt.Println("run make")
+
+	// create db
 	query_create := "CREATE DATABASE IF NOT EXISTS " + db.Name
 	query_use := "USE " + db.Name
-	ctx :=context.Background()
-	rows, err := db.Godb.QueryContext(ctx, query_create)
-	if err != nil {
-		log.Fatal(err)
-		fmt.Println(err)
-	}
-	defer rows.Close()
-	fmt.Println(rows)
-
-	rows, err = db.Godb.QueryContext(ctx, query_use)
-	if err != nil {
-		log.Fatal(err)
-		fmt.Println(err)
-	}
-	fmt.Println(rows)
-
-	query :="SELECT *	FROM `help_topic`	LIMIT 50"
-	rows, err = db.Godb.Query(query)
-	if err != nil {
-		log.Fatal(err)
-		fmt.Println(err)
-	}
-	fmt.Println(rows)
-
-
-	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	d := time.Now().Add(shortDuration)
+	ctx, cancel := context.WithDeadline(context.Background(), d)
 	defer cancel()
+	tx, err := db.Godb.BeginTx(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := tx.Exec(query_create)
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(err)
+	}
+	fmt.Println(rows)
+	rows, err = tx.Exec(query_use)
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(err)
+	}
+	fmt.Println(rows)
 
-	query_table:="CREATE TABLE Persons (		PersonID int,		LastName varchar(255),		FirstName varchar(255),		Address varchar(255),		City varchar(255)	);"
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// query := "SELECT *	FROM `help_topic`	LIMIT 50"
+	// rows, err = db.Godb.Query(query)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(rows)
+
+	// ctx, cancel = context.WithTimeout(ctx, 1*time.Second)
+	// defer cancel()
+
+	query_table := "CREATE TABLE Persons (		PersonID int,		LastName varchar(255),		FirstName varchar(255),		Address varchar(255),		City varchar(255)	);"
 	if ctx, err := db.Godb.QueryContext(ctx, query_table); err != nil {
 		log.Fatalf("unable to connect to database: %v", err)
 		fmt.Println(ctx)
@@ -89,7 +102,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-    err=db.Ping();
+	err = db.Ping()
 	// defer sql.Close()
 
 	// rows, err := db.Query("select id, first_name from user limit 10")
